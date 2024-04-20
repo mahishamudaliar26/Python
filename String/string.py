8ebb4778b7b8d543afd383362d2c2cee1e5419c1
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-04-20 12:18:15 +0530

    Strings

---
+++ String/string.py
@@ -0,0 +1,7 @@
+name = "Mahisha Mudaliar"
+print(name.upper())
+print(name.lower())
+print(name.find('Mud'))  # Returns index value starting from 0
+print(name.replace("Mahisha", "Harini"))
+print('h'in name)
+print('z'in name)
