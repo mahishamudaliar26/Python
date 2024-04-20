dfe2be37ef07ccd28829f2f37a55dec1b93cf671
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-04-20 11:08:30 +0530

    Variables

---
+++ Variables/variables.py
@@ -0,0 +1,20 @@
+name = "Mahisha"  # String 
+age = 22        # Integer
+height = 163.0  # Float
+is_adult = True  #bool
+print (name) 
+print(age)
+
+
+#Exercise 
+username = input("Enter username: ")
+age = input("Enter age: ")
+result = "Person name is: " + username
+result2 = username + "'s age is " + age
+print(result)
+print(result2)
+
+
+#Exercise 2 
+name2 = input("What is your superhero name? ")
+print(name2 + " is secretly a superhero. ")
