4efe9de434d99962c3bd2c79ae617788e455058d
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-04-20 11:38:32 +0530

    Typecasting

---
+++ Type Casting/typecasting.py
@@ -0,0 +1,16 @@
+#Implicit type casting
+oldage = input("Enter your age: ")
+newage = int(oldage) + 10
+print(newage)
+
+
+#Conversions: 
+# float() 
+# str()
+# bool()
+
+first = input("Enter first number: ")
+second = input("Enter second number: ")
+new_age = int(first) +int(second)
+print(new_age)
+print("The sum of age is: " + str(new_age))
