079a4bab7acbdde73e70da779bf2438b923baa56
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-04-20 19:40:10 +0530

    Loops

---
+++ Loops/ForLoop.py
@@ -0,0 +1,21 @@
+for i in range(5):
+    print(i)
+
+print("------------------------------------------")
+
+
+x = ["Mahisha", "Sahil", "Anup", "Dheeraj", "Shubham"]
+for i in x:
+    print(i)
+
+print("------------------------------------------")
+
+y = 5
+for y in range(5,10):
+    print(y)
+
+print("------------------------------------------")
+
+y = 5
+for y in range(5,10):
+    print(y+1)
---
+++ Loops/WhileLoop.py
@@ -0,0 +1,9 @@
+i = 0
+while i<= 5:
+    i += 1
+    print(i * "*")
+
+i = 5
+while i>= 0:
+    i -= 1
+    print(i * "*")
