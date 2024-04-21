1ea803719426b8b9958441e5525d951340b7a893
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-04-21 18:27:50 +0530

    Break and continue

---
+++ List/breakandcont.py
@@ -0,0 +1,13 @@
+students = ["ram", "shyaam", "kishan", "radha", "radhika"]
+for i in students:
+    if i == "radha":
+        break
+    print(i)
+
+print("-------------------------------------------------------")    
+
+for i in students:
+    if i == "radha":
+        continue
+    print(i)
+
