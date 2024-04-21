a3e279268069013202fbf6ba438870fbaefe5da5
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-04-21 18:35:10 +0530

    Tuples

---
+++ Tuple/tuple.py
@@ -0,0 +1,7 @@
+# Immutable: once created, elements cannot be 
+# added or removed.
+
+marks = (99,99, 98, 97, 100)
+
+print(marks.count(99))
+print(marks.index(99))  #returns index value of first appearing value
