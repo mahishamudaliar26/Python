fb9290b220e710f20be6d5b8af0013e931001f48
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-04-21 18:44:07 +0530

    Sets

---
+++ Set/sets.py
@@ -0,0 +1,7 @@
+marks = {95, 96, 96, 97, 98, 99, 100}
+print(marks)  # By default sets filters out the duplicate value.
+
+# They dont have index value and hence they are unordered
+
+for score in marks:
+    print(score)
