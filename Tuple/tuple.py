7e745821bff189119c2dad7426ba58876378b59e
mahishamudaliar26 <mahishamudaliar2603@gmail.com>
2024-04-21 18:40:05 +0530

    Tuples

--- Tuple/tuple.py
+++ Tuple/tuple.py
@@ -1,6 +1,8 @@
 # Immutable: once created, elements cannot be 
 # added, removed or edited form the tuple.
 
+# There is no need of braces, by default without braces ele,emts would be considered as tuples.
+
 marks = (99,99, 98, 97, 100)
 
 print(marks.count(99))
